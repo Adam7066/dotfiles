ecebd36fd5a190c3fc7d98dabacd16d6abe77266
Smallten <smallten@smallten.me>
2026-02-09 12:40:25 +0800

    feat: add git

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 # symlink
 if [[ $(uname) == "Darwin" ]]; then
+    # ghostty
     ln -sf "$(pwd)/ghostty/config" "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
     ln -sf "$(pwd)/ghostty/macos.config" "$HOME/Library/Application Support/com.mitchellh.ghostty/macos.config"
+
+    # git
+    ln -sf "$(pwd)/git/.gitconfig" "$HOME/.gitconfig"
 fi
